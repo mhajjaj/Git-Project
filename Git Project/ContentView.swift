86567a8446f2798a461a4f83c779e45f49ef9ae5
mhajjaj <mhajjaj7@gmail.com>
2023-11-27 12:27:32 +0900

    Initial Commit

---
+++ Git Project/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Git Project
+//
+//  Created by rutilea on 2023/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isPresented = false
+    var body: some View {
+        NavigationView {
+            Button("Show Model") {
+                self.isPresented = true
+            }.sheet(isPresented: $isPresented) {
+                Text("This is the model screen")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Git Project/Git_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_ProjectApp.swift
+//  Git Project
+//
+//  Created by rutilea on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct Git_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
